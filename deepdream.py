bbc15f8cadaeb11836c942f2206e05c9bd66d466
Nilvalox <nilotpalm2015@gmail.com>
2026-05-26 13:31:18 +0530

    using googlenet instead of inception net for this as its better

--- deepdream.py
+++ deepdream.py
@@ -9,6 +9,23 @@ import PIL.Image
 #test picture 
 url = ''
 
+
+#choosing model , lets choose googlenet for this 
+
+base_model = tf.keras.applications.GoogLeNet(include_top=False, weights='imagenet')
+
+#as googlenet is also based off inception net , same layers are present in it so can be modified
+
+#Max activations of the layers named mixed3 and mixed 5
+
+names = ['mixed3', 'mixed5']
+layers = [base_model.get_layer(name).output for name in names']
+          
+# creating a feature extraction model named dream_model
+dream_model = tf.keras.Model(inputs=base_model.input, outputs=layers)
+
+#
+
 class DeepDream(tf.module)
     def __init__(self, model):
     self.model = model
